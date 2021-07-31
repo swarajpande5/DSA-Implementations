27dc4e33b0b5fd7975ca4c05ac7ec9690c48a384
swarajpande4 <swarajpande4@gmail.com>
2021-07-31 18:08:24 +0530

    Added Topological Sort in Graphs

---
+++ Graphs/topologicalSort.cpp
@@ -0,0 +1,79 @@
+// Topological sort of a directed graph 
+#include <bits/stdc++.h>
+using namespace std; 
+
+class Graph 
+{
+    int nodes;
+    vector<int> *adjList;
+
+    void topologicalSortUtil(int v, bool visited[], stack<int> &st);
+
+public:
+    Graph(int inNodes);  
+    ~Graph();  
+    void addEdge(int src, int dest);  
+
+    void topologicalSort();
+};
+
+Graph::Graph(int inNodes)
+{
+    nodes = inNodes;
+    adjList = new vector<int>[inNodes];
+}
+
+Graph::~Graph()
+{
+    delete[] adjList;
+}
+
+void Graph::addEdge(int src, int dest)
+{
+    adjList[src].push_back(dest);   
+}
+
+void Graph::topologicalSortUtil(int v, bool visited[], stack<int> &st)
+{
+    visited[v] = true;
+
+    for(auto i = adjList[v].begin(); i != adjList[v].end(); i++)
+        if(!visited[*i])
+            topologicalSortUtil(*i, visited, st);
+    st.push(v);
+}
+
+void Graph::topologicalSort()
+{
+    stack<int> st;
+
+    bool visited[nodes];
+    for(int i = 0; i < nodes; i++)
+        visited[i] = false;
+    
+    for(int i = 0; i < nodes; i++)
+        if(visited[i] == false)
+            topologicalSortUtil(i, visited, st);
+    
+    while(st.empty() == false)
+    {
+        cout << st.top() << " ";
+        st.pop();
+    }
+    cout << "\n";
+}
+
+int main()
+{
+    Graph g(6);
+    g.addEdge(5, 2);
+    g.addEdge(5, 0);
+    g.addEdge(4, 0);
+    g.addEdge(4, 1);
+    g.addEdge(2, 3);
+    g.addEdge(3, 1);
+
+    g.topologicalSort();
+
+    return 0;
+}
