7d2949dd1ceed8d36dd0df9bb48cfc950d866ffa
swarajpande4 <swarajpande4@gmail.com>
2021-11-10 20:23:51 +0530

    Added Coin Change 1

---
+++ Dynamic Programming/coinChange1.cpp
@@ -0,0 +1,42 @@
+// Coin Change - 1 (Maximum number of ways) (Unbounded Knapsack variation) : https://practice.geeksforgeeks.org/problems/coin-change2448/1
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxWays(int coins[], int n, int sum)
+{
+    int t[n + 1][sum + 1];
+
+    // Initialization
+    for(int i = 0; i < sum + 1; i++)
+        t[0][i] = 0;
+    for(int i = 0; i < n + 1; i++)
+        t[i][0] = 1;
+
+    for(int i = 1; i < n + 1; i++)
+    {
+        for(int j = 1; j < sum + 1; j++)
+        {
+            if(coins[i - 1] <= j)
+                t[i][j] = t[i - 1][j] + t[i][j - coins[i - 1]];
+            else 
+                t[i][j] = t[i - 1][j];
+        }
+    }
+
+    return t[n][sum];
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int coins[n];
+    for(int i = 0; i < n; i++)
+        cin >> coins[i];
+    int sum;
+    cin >> sum;
+
+    cout << maxWays(coins, n, sum) << "\n";
+
+    return 0;
+}
