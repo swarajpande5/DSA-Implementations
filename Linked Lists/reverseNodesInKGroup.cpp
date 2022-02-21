fd93b0c322dfd01ebeda39810db3e9ac27dcf2d7
swarajpande4 <swarajpande4@gmail.com>
2022-02-21 20:58:28 +0530

    Added recursive solution in reverse nodes in k groups

--- Linked Lists/reverseNodesInKGroup.cpp
+++ Linked Lists/reverseNodesInKGroup.cpp
@@ -1,4 +1,57 @@
 // Reverse Nodes in k-Group : https://leetcode.com/problems/reverse-nodes-in-k-group/
+// Alter of above solution
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+// Recursive solution
+class Solution 
+{
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) 
+    {
+        if(!head)
+            return NULL;
+        
+        ListNode *kSizeCheck = head;
+        
+        for(int i = 0; i < k; i++)
+        {
+            if(kSizeCheck == NULL)
+                return head;
+            
+            kSizeCheck = kSizeCheck -> next;
+        }
+        
+        int count = 0;
+        ListNode *curr = head;
+        ListNode *prev = NULL;
+        ListNode *next = NULL;
+        
+        while(curr && count < k)
+        {
+            next = curr -> next;
+            curr -> next = prev;
+            prev = curr;
+            curr = next;
+            count++;
+        }
+        
+        if(next)
+            head -> next = reverseKGroup(next, k);
+        
+        return prev;
+    }
+};
+
+// Iterative Solution
 class Solution 
 {
 public:
@@ -36,6 +89,63 @@ public:
             pre = curr;
             count -= k;
         }
+        return dummy -> next;
+    }
+};
+
+// Alternative Iterative Solution
+class SolutionAlter 
+{
+private:
+    int getLengthOfList(ListNode *head)
+    {
+        ListNode *ptr = head;
+        int count = 0;
+        while(ptr)
+        {
+            count++;
+            ptr = ptr -> next;
+        }
+        
+        return count;
+    }
+    
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) 
+    {
+        if(!head)
+            return NULL;
+        
+        int len = getLengthOfList(head);
+        
+        if(len < k)
+            return head;
+    
+        int groups = len / k;
+        
+        ListNode *dummy = new ListNode(-1);
+        dummy -> next = head;
+        
+        ListNode *start = dummy;
+        ListNode *pre, *rem, *next;
+        
+        for(int i = 0; i < groups; i++)
+        {
+            pre = NULL;
+            rem = head;
+            for(int j = 0; j < k; j++)
+            {
+                next = head -> next;
+                head -> next = pre;
+                pre = head;
+                head = next;
+            }
+            
+            start -> next = pre;
+            rem -> next = head;
+            start = rem;
+        }
+        
         return dummy -> next;
     }
 };
