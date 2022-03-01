e26d2821e61c0a9092baa5faa76b08f444eb5ada
swarajpande4 <swarajpande4@gmail.com>
2022-03-01 09:38:41 +0530

    Fixed find all anagrams in strnig

--- Strings/findAllAnagramsInString.cpp
+++ Strings/findAllAnagramsInString.cpp
@@ -1,36 +1,47 @@
 // Find all anagrams in string: https://leetcode.com/problems/find-all-anagrams-in-a-string/submissions/
+//                              https://leetcode.com/problems/find-all-anagrams-in-a-string/discuss/1305008/Simple-C%2B%2B-solutionororSliding-WindowororWith-explanation
 
 class Solution
 {
 public:
     vector<int> findAnagrams(string s, string p)
     {
-        int n = s.length();
-        int m = p.length();
+        vector<int> res;
 
-        if(m > n)
-            return {};
-        
-        vector<int> pv(26, 0), sv(26, 0), res;
+        unordered_map<char, int> mp;
 
-        for(int i = 0; i < m; i++)
-            pv[p[i] - 'a']++;
+        for(int i = 0; i < p.length(); i++)
+            mp[p[i]]++;
         
+        int count = mp.size();
+        int k = p.length();         // Window size
+
         int i = 0, j = 0;
-        while(j < n)
+        while(j < s.length())
         {
-            sv[s[i] - 'a']++;
+            if(mp.find(s[j]) != mp.end())       // If a character is found which is present in map, then decrease the freq by 1
+            {
+                mp[s[j]]--;
+                if(mp[s[j]] == 0)
+                    count--;
+            }
 
-            if((j - i + 1) < m)             // If the current window size is smaller we move ahead
+            if(j - i + 1 < k)
                 j++;
             
-            else if((j - i + 1) == m)
+            else if(j - i + 1 == k)
             {
-                if(pv == sv)
+                if(count == 0)
                     res.push_back(i);
-                
-                sv[s[i] - 'a']--;
-                
+
+                if(mp.find(s[i]) != mp.end())
+                {
+                    if(mp[s[i]] == 0)
+                        count++;
+
+                    mp[s[i]]++;
+                }
+
                 i++;
                 j++;
             }
