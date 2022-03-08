7ef6a1630abfd8bc508af9ea04b34f056e360627
swarajpande4 <swarajpande4@gmail.com>
2022-03-08 21:48:41 +0530

    Added 1d dp solution for target sum

--- Dynamic Programming/targetSum.cpp
+++ Dynamic Programming/targetSum.cpp
@@ -57,3 +57,32 @@ int main()
 
     return 0;
 }
+
+
+// Leetcode Problem: https://leetcode.com/problems/target-sum/
+//                   https://leetcode.com/problems/target-sum/discuss/1174595/99-faster-oror-Using-DP-oror-Formula-explain
+
+class Solution 
+{
+public:
+    int findTargetSumWays(vector<int>& nums, int target) 
+    {
+        
+        int n = nums.size();
+        target = abs(target);
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        int diff = (sum + target) / 2;
+        
+        if(sum < target || (sum + target) % 2 != 0)
+            return 0;
+    
+        vector<int> dp(diff + 1, 0);
+        dp[0] = 1;
+        
+        for(int i = 0; i < n; i++)
+            for(int j = diff; j >= nums[i]; j--)
+                dp[j] += dp[j - nums[i]];
+    
+        return dp[diff];
+    }
+};
