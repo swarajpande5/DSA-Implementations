1d2b24c0bba3124b3acb9c54d5858731deb212d0
swarajpande4 <swarajpande4@gmail.com>
2022-04-01 20:39:46 +0530

    Added another solution in remove duplicate letters

--- Strings/removeDuplicateLetters.cpp
+++ Strings/removeDuplicateLetters.cpp
@@ -5,6 +5,48 @@ class Solution
 public:
     string removeDuplicateLetters(string s) 
     {
+        vector<int> index(26, -1);
+        set<char> visited;
+        stack<char> st;
+        
+        int n = s.size();
+        
+        for(int i = 0; i < n; i++)
+            index[s[i] - 'a'] = i;
+        
+        for(int i = 0; i < n; i++)
+        {
+            if(visited.count(s[i]))
+                continue;
+            
+            while(!st.empty() && s[i] < st.top() && i < index[st.top() - 'a'])
+            {
+                visited.erase(st.top());
+                st.pop();
+            }
+            
+            st.push(s[i]);
+            visited.insert(s[i]);
+        }
+        
+        string res = "";
+        while(!st.empty())
+        {
+            res += st.top();
+            st.pop();
+        }
+        
+        reverse(res.begin(), res.end());
+        
+        return res;
+    }
+};
+
+class Solution2 
+{
+public:
+    string removeDuplicateLetters(string s) 
+    {
         unordered_map<char, int> freq;
         unordered_map<char, bool> visited;
         string res;
