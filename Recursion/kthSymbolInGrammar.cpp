56ba5359208570fa48ec6c7e2fbd180982b40902
swarajpande4 <swarajpande4@gmail.com>
2022-04-20 19:53:06 +0530

    Added kth Symbol in Grammar

---
+++ Recursion/kthSymbolInGrammar.cpp
@@ -0,0 +1,17 @@
+// kth Symbol in grammar: https://leetcode.com/problems/k-th-symbol-in-grammar/
+
+class Solution 
+{
+public:
+    int kthGrammar(int n, int k) 
+    {
+        if(n == 1 || k == 1)
+            return 0;
+        
+        int len = pow(2, n - 2);
+        if (k <= len)
+            return kthGrammar(n - 1, k);
+        
+        return not kthGrammar(n - 1, k - len);
+    }
+};
