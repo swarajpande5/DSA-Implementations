7e5c01f6a220f30444d4f991d05b2a617f81e23d
swarajpande4 <swarajpande4@gmail.com>
2022-04-23 19:31:25 +0530

    Added optimal solution in n queens

--- Backtracking/nQueens.cpp
+++ Backtracking/nQueens.cpp
@@ -53,3 +53,54 @@ public:
         return res;
     }
 };
+
+
+// Optimal Solution
+class Solution 
+{
+public:
+    
+    vector<vector<string>> res;
+    
+    bool isValid(vector<string> &board, int row, int col, set<int> &colSet, set<int> &diagSet, set<int> &antidiagSet)
+    {
+        return !colSet.count(col) && !diagSet.count(row - col) && !antidiagSet.count(row + col);
+    }
+    
+    void dfs(vector<string> &board, int row, set<int> &colSet, set<int> &diagSet, set<int> &antidiagSet)
+    {
+        if(row == board.size())
+        {
+            res.push_back(board);
+            return;
+        }
+        
+        // Iterating every possible position
+        for(int i = 0; i < board.size(); i++)
+        {
+            if(isValid(board, row, i, colSet, diagSet, antidiagSet))
+            {
+                board[row][i] = 'Q';
+                colSet.insert(i);
+                diagSet.insert(row - i);
+                antidiagSet.insert(row + i);
+                
+                dfs(board, row + 1, colSet, diagSet, antidiagSet);          // Recursive call
+                
+                board[row][i] = '.';                                        // Backtracking
+                colSet.erase(i);
+                diagSet.erase(row - i);
+                antidiagSet.erase(row + i);
+            }
+        }
+    }
+    
+    vector<vector<string>> solveNQueens(int n) 
+    {
+        vector<string> board(n, string(n, '.'));
+        
+        set<int> colSet, diagSet, antidiagSet;
+        dfs(board, 0, colSet, diagSet, antidiagSet);
+        return res;
+    }
+};
