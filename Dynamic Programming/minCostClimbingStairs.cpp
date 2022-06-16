f1ee3a3410f827b807698dab50f4b77bb234c074
swarajpande4 <swarajpande4@gmail.com>
2022-06-16 20:32:04 +0530

    fixed min cost climbing stairs

--- Dynamic Programming/minCostClimbingStairs.cpp
+++ Dynamic Programming/minCostClimbingStairs.cpp
@@ -23,19 +23,6 @@ public:
     }
 };
 
-// Bottom up DP
-class BottomUpSolution
-{
-public:
-    int minCostClimbingStairs(vector<int> &cost)
-    {
-        for (int i = 2; i < cost.size(); i++)
-            cost[i] += min(cost[i - 1], cost[i - 2]);
-
-        return min(cost[cost.size() - 1], cost[cost.size() - 2]);
-    }
-};
-
 // Space Optimized Bottom up DP 
 class Solution
 {
