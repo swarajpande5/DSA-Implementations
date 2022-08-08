f8f1b758182ba8ee9e2f63e8bf6e3270f7e613d1
swarajpande4 <swarajpande4@gmail.com>
2022-08-08 11:16:31 +0530

    Added code for all possible solutions of N-Queens

--- Backtracking/nQueens.cpp
+++ Backtracking/nQueens.cpp
@@ -104,3 +104,69 @@ public:
         return res;
     }
 };
+
+
+// GFG Question: https://practice.geeksforgeeks.org/problems/n-queen-problem0315/1
+// To print all possible solution of the N-Queens problem 
+
+class Solution
+{
+public:
+
+    vector<vector<int>> res; 
+    
+    bool isValid(vector<vector<int>> &board, int row, int col, set<int> &colSet, set<int> &diagSet, set<int> &antidiagSet)
+    {
+        return !colSet.count(col) && !diagSet.count(row - col) && !antidiagSet.count(row + col);
+    }
+
+    bool dfs(vector<vector<int>> &board, int row, set<int> &colSet, set<int> &diagSet, set<int> &antidiagSet)
+    {
+        if(row == board.size())
+        {
+            vector<int> v;
+            for(int i = 0; i < board.size(); i++)
+                for(int j = 0; j < board[0].size(); j++)
+                    if(board[i][j] == 1)
+                        v.push_back(j + 1);
+            
+            res.push_back(v);
+            return true;
+        }
+        
+        bool check = false; 
+        for(int i = 0; i < board.size(); i++)
+        {
+            if(isValid(board, row, i, colSet, diagSet, antidiagSet))
+            {
+                board[row][i] = 1;
+                colSet.insert(i);
+                diagSet.insert(row - i);
+                antidiagSet.insert(row + i);
+                
+                check = dfs(board, row + 1, colSet, diagSet, antidiagSet) || check;          // Recursive call
+                
+                board[row][i] = 0;                                        // Backtracking
+                colSet.erase(i);
+                diagSet.erase(row - i);
+                antidiagSet.erase(row + i);
+            }
+        }
+        
+        return check;
+    }
+
+    vector<vector<int>> nQueen(int n) 
+    {
+        res.clear(); 
+        vector<vector<int>> board(n, vector<int>(n, 0));
+        set<int> colSet, diagSet, antidiagSet;
+        
+        if(dfs(board, 0, colSet, diagSet, antidiagSet) == false)
+            return {};
+        
+        sort(res.begin(), res.end());
+        return res;
+        
+    }
+};
